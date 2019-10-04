a632a1c253485ddc213c0f894590d1f3b5fc5ef6
QuantumSheep <quantumsheep@outlook.fr>
2019-10-04 16:03:55 +0200

    Use taco::dialog::run() in dialog test

--- tests/dialog/src/main.cpp
+++ tests/dialog/src/main.cpp
@@ -1,55 +1,22 @@
 #include <taco/taco.hpp>
 
 #include <iostream>
-#include <string>
-#include <vector>
 
 int main()
 {
     taco::dialog dialog("Hello!");
 
-    bool running = true;
-
-    dialog.add_possibility("Yes", [&] {
-        running = false;
+    dialog.add_possibility("Yes", [] {
         std::cout << "Yes" << std::endl;
     });
 
-    dialog.add_possibility("No", [&] {
-        running = false;
+    dialog.add_possibility("No", [] {
         std::cout << "No" << std::endl;
     });
 
-    dialog.add_possibility("Quit", [&] {
-        running = false;
-    });
+    dialog.add_possibility("Quit", [] {});
 
-    while (running)
-    {
-        dialog.display();
-
-        char touch = taco::getch();
-
-        if (touch == taco::input::up_arrow)
-        {
-            dialog.up();
-        }
-
-        if (touch == taco::input::down_arrow)
-        {
-            dialog.down();
-        }
-
-        if (touch == taco::input::enter)
-        {
-            dialog.execute_selected();
-        }
-
-        if (touch == taco::input::ctrl_c)
-        {
-            running = false;
-        }
-    }
+    dialog.run();
 
     return 0;
 }
